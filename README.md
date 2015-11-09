451de942cf4d37f1e6e79da1ae31fc3129fd4092
craigmj <craig@lateral.co.za>
2015-11-09 15:35:16 +0200

    Separation to support MySQL and PostgreSQL databases

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ gogo
 Gogo handles database migrations in Go code like yoyo-migrate for python (well, a little like that).
 
 It has been tested with MySQL and with SQLITE3 driver.
+
+USAGE
+=====
+
+
--- gogo.go
+++ gogo.go
@@ -1,19 +1,19 @@
 package gogo
 
 import (
-	"fmt"
 	"database/sql"
-	"strconv"
-	"time"
+	"fmt"
+	"log"
 	"reflect"
+	"strconv"
 )
 
 var db *sql.DB
 var gogoTable = "_gogo_migrations"
-	
+
 // A Migration is a one-way advancement of the database.
-type Migration struct  {
-	Apply func(*Tx)
+type Migration struct {
+	Apply    func(*Tx)
 	Rollback func(*Tx)
 }
 
@@ -24,42 +24,33 @@ func MetaTable(table string) {
 }
 
 // Version returns the current version of the database.
-// Your migrations are numbered from 1, so a version of 0 means that no 
+// Your migrations are numbered from 1, so a version of 0 means that no
 // migrations have been applied.
 func Version(db *sql.DB) (int, error) {
 	var version int
 
-	var tblname string
-	res := db.QueryRow(`show tables like '` + gogoTable + `'`)
-	err := res.Scan(&tblname)
-	if nil!=err && sql.ErrNoRows!=err{
-		//return -1, fmt.Errorf("Executing `show tables like '%v'`: %v", gogoTable, err.Error())
-		err = sql.ErrNoRows
+	hasGogoTable, err := dbi.TableExists(db, gogoTable)
+	if nil != err {
+		return 0, err
 	}
-	if sql.ErrNoRows == err {
-		_, err = db.Exec(fmt.Sprintf(`
-			create table if not exists %s
-			(
-				version int not null default 0,
-				migration_date datetime not null, 
-				primary key(version)
-			)`, gogoTable))
-		if nil!=err {
+	if !hasGogoTable {
+		_, err = db.Exec(dbi.CreateGogoTableSql(gogoTable))
+		if nil != err {
 			return -1, fmt.Errorf("Creating gogomigration versioning table %v: %v", gogoTable, err.Error())
 		}
-
 		version = 0
 	}
-	res = db.QueryRow(`select version from ` + gogoTable)
+	res := db.QueryRow(dbi.SelectVersionSql(gogoTable))
 	err = res.Scan(&version)
 	if sql.ErrNoRows == err {
-		_, err = db.Exec(fmt.Sprintf(`insert into %s(version, migration_date) values(0, ?)`, gogoTable), time.Now())
-		if nil!=err {
+
+		_, err = db.Exec(dbi.InsertVersionSql(gogoTable), 0)
+		if nil != err {
 			return -1, fmt.Errorf("Inserting 0 version into %v: %v", gogoTable, err.Error())
 		}
 		return version, nil
 	}
-	if nil!=err {
+	if nil != err {
 		return -1, fmt.Errorf("Scanning version from %v: %v", gogoTable, err.Error())
 	}
 
@@ -68,130 +59,132 @@ func Version(db *sql.DB) (int, error) {
 
 // Apply migrates the database to the latest migration version
 func Migrate(db *sql.DB, migrations []Migration) (err error) {
-	db.Exec(`set autocommit=0`)
-	defer func() {
-		db.Exec(`set autocommit=1`)		
-	}()
-	
-	version, err := Version(db)
-	if nil!=err {
+	err = dbi.PreMigrate(db)
+	if nil != err {
 		return err
 	}
-	
+	defer dbi.PostMigrate(db)
+
+	version, err := Version(db)
+	if nil != err {
+		return err
+	}
+
 	// safeExec applies the migration, and returns an error if the migration returns
 	// an error, or if the migration panics
 	safeExec := func(tx *sql.Tx, f func(*Tx)) (err error) {
 		defer func() {
-			if e := recover(); e!=nil {
+			if e := recover(); e != nil {
 				ok := true
 				err, ok = e.(error)
 				if !ok {
 					err = fmt.Errorf("Unexpected panic value of type %v: %v", reflect.TypeOf(e).Name(), e)
 				}
 			}
-		}()		
+		}()
 		f(&Tx{tx})
 		return nil
 	}
-	
+
 	onError := func(tx *sql.Tx, ver int, err error) error {
-		if err := safeExec(tx, migrations[ver].Rollback); nil!=err {
-			fmt.Println("ERROR during migration.Rollback: ", err)
+		log.Println("onError ", err, " : on ver=", ver)
+		if err := safeExec(tx, migrations[ver].Rollback); nil != err {
+			log.Println("ERROR during migration.Rollback: ", err)
 		}
-		if err := tx.Rollback(); nil!=err {
-			fmt.Println("ERROR during transation rollback: ", err)
+		if err := tx.Rollback(); nil != err {
+			log.Println("ERROR during transaction rollback: ", err)
 		}
-		return fmt.Errorf("Migrating to version %v: %v", version+1, err.Error()) 
+		return fmt.Errorf("Migrating to version %v: %v", version+1, err.Error())
 	}
 	for version < len(migrations) {
 		tx, err := db.Begin()
-		if nil!=err {
+		if nil != err {
 			return fmt.Errorf("Starting transaction: %v", err.Error())
 		}
-		if err := safeExec(tx, migrations[version].Apply); nil!=err {
+		if err := safeExec(tx, migrations[version].Apply); nil != err {
 			return onError(tx, version, err)
 		}
 		version++
-		_, err = tx.Exec(`update ` + gogoTable + ` set version = ?, migration_date=?`, version, time.Now())
-		if nil!=err {
+		_, err = tx.Exec(dbi.UpdateVersionSql(gogoTable), version)
+		if nil != err {
 			return onError(tx, version, fmt.Errorf("Updating version table for version %v: %s", version, err.Error()))
 		}
 		err = tx.Commit()
-		if nil!=err {
+		if nil != err {
 			return onError(tx, version, fmt.Errorf("Committing transaction for migration %v: %v", version+1, err.Error()))
 		}
 	}
 	return nil
 }
 
-// Rollback rolls the database back to the destination version. Versions are 
+// Rollback rolls the database back to the destination version. Versions are
 // numbered from 1, so a Rollback to 0 would rollback all migrations.
 func Rollback(db *sql.DB, destinationVersionString string, migrations []Migration) (err error) {
-	if ""==destinationVersionString {
+	if "" == destinationVersionString {
 		return nil
 	}
 	destinationVersion, err := strconv.Atoi(destinationVersionString)
-	if nil!=err {
+	if nil != err {
 		return err
 	}
 
-	db.Exec(`set autocommit=0`)
-	defer func() {
-		db.Exec(`set autocommit=1`)
-	}()
+	if err = dbi.PreMigrate(db); nil != err {
+		return err
+	}
+	defer dbi.PostMigrate(db)
 
 	version, err := Version(db)
-	if nil!=err {
+	if nil != err {
 		return err
 	}
-	
+
 	// Negative destinationVersions mean we rollback by a delta
-	if 0>destinationVersion {
+	if 0 > destinationVersion {
 		destinationVersion = version + destinationVersion
-		if 0>destinationVersion {
+		if 0 > destinationVersion {
 			return fmt.Errorf("Cannot rollback to negative version %d from current version %d", destinationVersion, version)
 		}
 	}
-	
-	fmt.Println("Rollback to version ", destinationVersion)
+
+	log.Println("Rollback to version ", destinationVersion)
 	// safeRollback rollsback the migration, and returns an error if the migration rollback returns
 	// an error, or if the migration rollback panics
 	safeRollback := func(tx *sql.Tx, migration Migration) (err error) {
 		defer func() {
-			if e := recover(); e!=nil {
+			if e := recover(); e != nil {
 				ok := true
 				err, ok = e.(error)
 				if !ok {
 					err = fmt.Errorf("Unexpected panic value of type %v: %v", reflect.TypeOf(e).Name(), e)
 				}
 			}
-		}()		
+		}()
 		migration.Rollback(&Tx{tx})
 		return nil
 	}
 
-	if 1>version {
+	if 1 > version {
 		return fmt.Errorf("Cannot rollback: currently at version %v", version)
 	}
-	
+
 	for version > destinationVersion {
-		fmt.Printf("Rolling back from version %v\n", version)
+		log.Println("Rolling back from version", version)
 		version--
 		Tx, err := db.Begin()
-		if nil!=err {
+		if nil != err {
 			return fmt.Errorf("Starting transaction: %v", err.Error())
 		}
-		if err := safeRollback(Tx, migrations[version]); nil!=err {
+		if err := safeRollback(Tx, migrations[version]); nil != err {
 			Tx.Rollback()
-			return fmt.Errorf("Rollingback version %v: %v", version+1, err.Error()) 
+			return fmt.Errorf("Rollingback version %v: %v", version+1, err.Error())
 		}
-		_, err = Tx.Exec(`update ` + gogoTable + ` set version = ?, migration_date=?`, version, time.Now())
-		if nil!=err {
-			Tx.Rollback()			
+		_, err = Tx.Exec(dbi.UpdateVersionSql(gogoTable), version)
+		if nil != err {
+			Tx.Rollback()
 			return fmt.Errorf("Updating version to %v: %v", version+1, err.Error())
 		}
 		err = Tx.Commit()
-		if nil!=err {
+		if nil != err {
 			Tx.Rollback()
 			return fmt.Errorf("Committing transaction for rollback %v: %v", version+1, err.Error())
 		}
